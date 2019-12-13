05799f52be25babb5007ad0ae857a2e6883eaa26
JessBob <54863103+Jessbob@users.noreply.github.com>
2019-12-13 09:13:19 -0600

    deploying to ghpages

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,118 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :style="pageContainer" id="wrapper">
+    <md-app md-mode="fixed" class="app-container">
+      <md-app-toolbar class="add-todo md-accent">
+        <md-field>
+          <md-input
+            v-model="currentTodo"
+            @keydown.enter="addTodo()"
+            placeholder="Add a ToDo"
+          ></md-input>
+        </md-field>
+      </md-app-toolbar>
+      <md-app-content>
+        <md-list class="todos">
+          <md-list-item v-for="todo in todos" :key="todo.id">
+            <span
+              class="todo-item"
+              :class="{ completed: todo.completed }"
+              @dblclick="editTodo(todo)"
+              v-if="!todo.edit"
+            >
+              {{ todo.label }}
+            </span>
+            <md-input
+              v-else
+              class="todo-edit"
+              type="text"
+              v-model="todo.label"
+              @keyup.enter="editDone(todo)"
+            >
+            </md-input>
+
+            <div class="todo-btns">
+              <md-button
+                class="completed-todo md-icon-button md-dense md-raised md-accent"
+                @click="todoFinished(todo)"
+              >
+                <md-icon>done</md-icon>
+              </md-button>
+              <md-button
+                class="remove-todo md-icon-button md-dense md-raised md-accent"
+                @click="removeTodo(todo)"
+              >
+                <md-icon>delete</md-icon>
+              </md-button>
+            </div>
+          </md-list-item>
+        </md-list>
+      </md-app-content>
+    </md-app>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      pageContainer: {
+        backgroundColor: "black",
+        padding: "20px"
+      },
+      todos: [],
+      currentTodo: "",
+      editedTodo: null
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        id: this.todos.length,
+        label: this.currentTodo,
+        completed: false,
+        edit: false
+      });
+      this.currentTodo = "";
+    },
+    removeTodo(todo) {
+      var index = this.todos.indexOf(todo);
+      this.todos.splice(index, 1);
+    },
+    editTodo(todo) {
+      todo.edit = true;
+    },
+    editDone(todo) {
+      todo.edit = false;
+    },
+    todoFinished(todo) {
+      todo.completed = true;
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.completed {
+  text-decoration: line-through;
+  background-color: rgba(97, 97, 97, 0.4);
+}
+
+.app-container {
+  max-width: 500px;
+  min-width: 100px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: auto;
+  height: 94vh;
+  border-radius: 5px;
+}
+.add-todo {
+  height: 100px;
+}
+.todo-btns {
+  float: right;
+  max-width: 15%;
+}
+.todo-item {
+  max-width: 60%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+//import { MdField } from "vue-material/dist/components";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/black-green-light.css";
+Vue.use(VueMaterial);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+/*new Vue(
+  {
+    data() {
+      return {
+        pageContainer: {
+          backgroundColor: "black",
+          padding: "20px"
+        }
+      };
+    },
+    render: h => h(App)
+  }.$mount("#app")
+);*/
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
